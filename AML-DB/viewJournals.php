a7fe897073840e7c60d7f0afaf0a5ff969a95ea6
Neal <Neal@192.168.1.102>
2014-12-07 10:44:51 -0900

    Applied

---
+++ AML-DB/viewJournals.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Journals</title>
+</head>
+
+<body>
+<p><strong><em>Journals Lookup
+</em></strong></p>
+<p>&nbsp;</p>
+<table width="400" border="0" cellspacing="2" cellpadding="2">
+  <tr>
+    <td width="180">Lookup By Publication ID:</td>
+    <td width="206"><label for="lookuppublicationid"></label>
+      <select name="lookuppublicationid" id="lookuppublicationid">
+    </select></td>
+  </tr>
+  <tr>
+    <td>Publication ID:</td>
+    <td><label for="publicationid"></label>
+    <input type="text" name="publicationid" id="publicationid" /></td>
+  </tr>
+  <tr>
+    <td>Publication Title:</td>
+    <td><form id="form1" name="form1" method="post" action="">
+      <label for="publicationtitle"></label>
+      <input type="text" name="publicationtitle" id="publicationtitle" />
+    </form></td>
+  </tr>
+  <tr>
+    <td>Frequency Code:</td>
+    <td><label for="frequencycode"></label>
+      <select name="frequencycode" id="frequencycode">
+    </select></td>
+  </tr>
+</table>
+<form id="form2" name="form2" method="post" action="">
+  <input type="submit" name="Edit Journel" id="Edit Journel" value="Edit Journel" />
+  <input type="submit" name="addnewjournel" id="addnewjournel" value="Add New Journel" />
+  <input type="submit" name="deletejournel" id="deletejournel" value="Delete Journel" />
+</form>
+<p>&nbsp;</p>
+</body>
+</html>
