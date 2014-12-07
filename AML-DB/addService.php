0e0967c5eb8b2aa9d85261fa3870aed727031bc3
briankapala <briankapala@gmail.com>
2014-12-06 15:21:34 -0900

    Update addService.php

--- AML-DB/addService.php
+++ AML-DB/addService.php
@@ -5,121 +5,174 @@
 <title>Untitled Document</title>
 </head>
 
+<?php
+$page_title = 'Services';
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require ('../connect_db.php');
+
+    $errors = array();
+
+    if (empty($_POST['servicesID'])) {
+        $errors[] = 'Enter a valid service.';
+    } else {
+        $sID = mysqli_real_escape_string($dbc, trim($_POST['serviceID']));
+    }
+
+    if (empty($_POST['dateFilled'])) {
+        $errors[] = 'Enter a valid fill date.';
+    } else {
+        $dF = mysqli_real_escape_string($dbc, trim($_POST['dateFilled']));
+    }
+
+    if (empty($_POST['identifier'])) {
+        $errors[] = 'Enter a valid identifier.';
+    } else {
+        $i = mysqli_real_escape_string($dbc, trim($_POST['identifier']));
+    }
+
+    if (empty($_POST['serviceCode'])) {
+        $errors[] = 'Enter a valid service code.';
+    } else {
+        $sC = mysqli_real_escape_string($dbc, trim($_POST['serviceCode']));
+    }
+
+    if (empty($_POST['quantityFilled'])) {
+        $errors[] = 'Enter a valid fill quantity.';
+    } else {
+        $qF = mysqli_real_escape_string($dbc, trim($_POST['serviceID']));
+    }
+
+    if (empty($_POST['unitPrice'])) {
+        $errors[] = 'Enter a valid unit price.';
+    } else {
+        $uP = mysqli_real_escape_string($dbc, trim($_POST['unitPrice']));
+    }
+
+    if (empty($_POST['total'])) {
+        $errors[] = 'Enter a valid service.';
+    } else {
+        $t = mysqli_real_escape_string($dbc, trim($_POST['total']));
+    }
+
+    if (empty($_POST['paid'])) {
+        $errors[] = 'Enter a valid if paid.';
+    } else {
+        $p = mysqli_real_escape_string($dbc, trim($_POST['paid']));
+    }
+
+    if (empty($_POST['dateEntered'])) {
+        $errors[] = 'Enter a valid entry date.';
+    } else {
+        $dE = mysqli_real_escape_string($dbc, trim($_POST['dateEntered']));
+    }
+
+    if (empty($errors)) {
+        $q = "INSERT INTO Services (ServicesID, DateFilled, Identifier, ServiceCode, QTYFilled, UnitPrice, Total, Paid, DateEntered) VALUES ('$sID', '$dF', '$i', '$sC', '$qF', '$uP', '$t', '$p', '$dE', NOW() )";
+        $r = @mysqli_query($dbc, $q);
+        if ($r) {
+            echo '<h1>Service added!</h1><p>The service has been added.</p><p><a href="services.php">Back to services</a></p>';
+        }
+
+        # Close database connection.
+        mysqli_close($dbc);
+
+        # Display footer section and quit script:
+        include ('includes/footer.html');
+        exit();
+    }
+    # Or report errors.
+    else {
+        echo '<h1>Error!</h1><p id="err_msg">The following error(s) occurred:<br>';
+        foreach ($errors as $msg) {
+            echo " - $msg<br>";
+        }
+        echo 'Please try again.</p>';
+        # Close database connection.
+        mysqli_close($dbc);
+    }
+}
+?>
+
 <body>
-<p><em><strong>Organizations Lookup</strong></em> </p>
+<p><strong><em>Add New Service</em></strong></p>
 <p>&nbsp;</p>
-<table width="746" border="0" cellspacing="2" cellpadding="2">
+<table width="585" border="0" cellspacing="2" cellpadding="2">
   <tr>
-    <td width="180">Lookup By Org Code</td>
-    <td width="180"><form id="form1" name="form1" method="post" action="">
-      <label for="lookuporgcode"></label>
-      <select name="lookuporgcode" id="lookuporgcode">
-      </select>
+    <td width="141">Services ID:</td>
+    <td width="168"><form id="form1" name="form1" method="post" action="">
+      <input type="text" name="servicesID" size="25" value="<?php if (isset($_POST['servicesID'])) echo $_POST['servicesID']; ?>" />
     </form></td>
-    <td width="180">Contact Information:</td>
-    <td width="180">&nbsp;</td>
+    <td width="105">&nbsp;</td>
+    <td width="145">&nbsp;</td>
   </tr>
   <tr>
-    <td>Organization Code:</td>
-    <td><input type="text" name="orgcode" id="orgcode" /></td>
-    <td>First Name</td>
-    <td><input type="text" name="firstname" id="firstname" /></td>
-  </tr>
-  <tr>
-    <td>Organization Name:</td>
-    <td><form id="form2" name="form2" method="post" action="">
-      <label for="orgname"></label>
-      <input type="text" name="orgname" id="orgname" />
-    </form></td>
-    <td>Last Name</td>
-    <td><input type="text" name="lastname" id="lastname" /></td>
-  </tr>
-  <tr>
-    <td>Physical Address:</td>
-    <td><input type="text" name="PhysicalAddress" id="PhysicalAddress" /></td>
-    <td>Title:</td>
-    <td><input type="text" name="title" id="title" /></td>
-  </tr>
-  <tr>
-    <td>City:</td>
-    <td><input type="text" name="City" id="City" /></td>
-    <td>Phone Number:</td>
-    <td><input type="text" name="phonenumber" id="phonenumber" /></td>
-  </tr>
-  <tr>
-    <td>State:</td>
-    <td><input type="text" name="state" id="state" /></td>
-    <td>Fax Number:</td>
-    <td><input type="text" name="faxnumber" id="faxnumber" /></td>
-  </tr>
-  <tr>
-    <td>Zip-Code:</td>
-    <td><input type="text" name="zipcode" id="zipcode" /></td>
+    <td>Date Filled:</td>
+    <td><input name="DateFilled" type="date" id="DateFilled" value="<?php if (isset($_POST['dateFilled'])) echo $_POST['dateFilled']; ?>" /></td>
     <td>&nbsp;</td>
     <td>&nbsp;</td>
   </tr>
   <tr>
-    <td>Billing Address:</td>
-    <td><strong>
-      <input type="text" name="Billing Address" id="Billing Address" />
-    </strong></td>
-    <td>Is Non-Profit:</td>
-    <td><p>
-      <label>
-        <input type="radio" name="isTC" value="Yes" id="isnonprofit" />
-        Yes</label>
-      <label>
-        <input type="radio" name="isTC" value="No" id="isTC_1" />
-        No</label>
-      <br />
-    </p></td>
+    <td>Identifier:</td>
+    <td><select name="Identifier" id="Identifier">
+      <option>&lt;?php if (isset($_POST['identifier'])) echo $_POST['identifier']; ?&gt;</option>
+    </select></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
   </tr>
   <tr>
-    <td>Billing City:</td>
-    <td><input type="text" name="Billing City" id="Billing City" /></td>
-    <td>Pays Fees:</td>
-    <td><p>
-      <label>
-        <input type="radio" name="isTC" value="Yes" id="paysfees" />
-        Yes</label>
-      <label>
-        <input type="radio" name="isTC" value="No" id="isTC_1" />
-        No</label>
-      <br />
-    </p></td>
+    <td>Service Code:</td>
+    <td><select name="select2" id="select2">
+      <option>&lt;?php if (isset($_POST['serviceCode'])) echo $_POST['serviceCode']; ?&gt;</option>
+    </select></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
   </tr>
   <tr>
-    <td>Billing State:</td>
-    <td><input type="text" name="Billing state" id="Billing state" /></td>
-    <td>Active:</td>
-    <td><form id="form3" name="form3" method="post" action="">
+    <td>Quantity Filled:</td>
+    <td><input name="QuantityFilled" type="text" id="QuantityFilled" value="<?php if (isset($_POST['quantityFilled'])) echo $_POST['quantityFilled']; ?>" /></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Unit Price:</td>
+    <td><input name="UnitPrice" type="text" id="UnitPrice" value="<?php if (isset($_POST['unitPrice'])) echo $_POST['unitPrice']; ?>" /></td>
+    <td>Profit Fee:</td>
+    <td><label for="ProfitFee"></label>
+    <input type="text" name="ProfitFee" id="ProfitFee" /></td>
+  </tr>
+  <tr>
+    <td>Total:</td>
+    <td><input name="Total" type="text" id="Total" value="<?php if (isset($_POST['total'])) echo $_POST['total']; ?>" /></td>
+    <td>Non-Profit Fee:</td>
+    <td><label for="NonProfitFee"></label>
+    <input type="text" name="NonProfitFee" id="NonProfitFee" /></td>
+  </tr>
+  <tr>
+    <td>Paid:</td>
+    <td><form id="form11" name="form11" method="post" action="">
       <p>
         <label>
-          <input type="radio" name="IsSDI" value="Yes" id="active" />
+          <input type="radio" name="RadioGroup1" value="<?php if (isset($_POST['paid'])) echo "checked"; ?> value = &quot;yes&quot;&gt;" id="RadioGroup1_0" />
           Yes</label>
-        <label>
-          <input type="radio" name="IsSDI" value="No" id="IsSDI_1" />
-          No</label>
-        <br />
+        <input name="RadioGroup1" type="radio" id="RadioGroup1_1" value="<?php if (isset($_POST['paid'])) echo "checked"; ?> value = &quot;no&quot;&gt;" />
+        No<br />
       </p>
     </form></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
   </tr>
   <tr>
-    <td>Billing Zip-Code:</td>
-    <td><input type="text" name="Billing zipcode" id="Billing zipcode" /></td>
-    <td>Notes:</td>
-    <td><input type="text" name="notes" id="notes" /></td>
+    <td>Date Entered:</td>
+    <td><input name="DateEntered" type="date" id="DateEntered" value="<?php if (isset($_POST['dateEntered'])) echo $_POST['dateEntered']; ?>" /></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
   </tr>
 </table>
-<p>Organizations:</p>
-<p>&nbsp;</p>
-<form id="form12" name="form12" method="post" action="">
-  <input type="submit" name="EditService" id="EditService" value="Edit Organization" />
-  <input type="submit" name="DeleteService" id="DeleteService" value="Delete Organization" />
-  <input type="submit" name="addNewService" id="addNewService" value="Add New Organization" />
+<form id="form2" name="form2" method="post" action="">
+  <input type="submit" name="Save Record" id="Save Record" value="Save Record" />
 </form>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
 <p>&nbsp;</p>
 </body>
 </html>
