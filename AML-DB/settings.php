8d1d57277ec1b7adb69df5cb943f0ae649c60a0f
Neal <Neal@192.168.1.102>
2014-12-07 17:36:09 -0900

    Deleted more extraneous files

--- AML-DB/settings.php
+++ AML-DB/settings.php
@@ -1,12 +1,18 @@
-<! />
+<html>
+    
+    <! Includes all miscellaneous record-oriented functions not included in the Journal, Service, SearchLog, People or Organization pages  />
+
+
+
+
+
+
+</html>
+
+
 
 
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 ?>
